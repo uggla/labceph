ed6468f5ecaf6c293f643b5536d3c345b56b8ea0
uggla <uggla@free.fr>
2015-06-26 13:27:30 +0200

    Update...

--- clean_volume.sh
+++ clean_volume.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-for i in $(virsh vol-list default | grep ceph | awk '{print $1}'); do virsh vol-delete --vol $i --pool default; done
-
 virsh detach-disk --domain cephosd1 vdb
 virsh detach-disk --domain cephosd2 vdb
 virsh detach-disk --domain cephosd3 vdb
@@ -9,3 +7,5 @@ virsh detach-disk --domain cephosd3 vdb
 virsh detach-disk --domain cephosd1 vdc
 virsh detach-disk --domain cephosd2 vdc
 virsh detach-disk --domain cephosd3 vdc
+
+for i in $(virsh vol-list default | grep ceph | awk '{print $1}'); do virsh vol-delete --vol $i --pool default; done
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 ansible-playbook deployCephVM.yml -i ansiblehost
+sleep 10s
 ansible-playbook configureOsd.yml -i ansiblehost
