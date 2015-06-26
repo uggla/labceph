a2fffd130ab99b8d95a206c1c2b4470e75d8e42a
uggla <uggla@free.fr>
2015-06-26 14:32:10 +0200

    Working cluster.

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,8 @@ sleep 10s
 ansible-playbook configureOsd.yml -i ansiblehost
 sleep 10s
 ansible-playbook installCeph.yml -i ansiblehost
+sleep 10s
+ansible-playbook updateKeyringRight.yml -i ansiblehost
+
+echo "Now you should have a working ceph cluster"
+exit 0
