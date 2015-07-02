7aca4950a2bc115cceee895e4955136273221e6f
cedric milesi <cedric.milesi@hp.com>
2015-07-02 17:55:29 +0200

    now the lab host preparation script is working (even when run as a simple mortal)

--- prepareHost.sh
+++ prepareHost.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo -s apt-get install ansible sshpass python-apt -y
+
+
 #ansible-playbook  prepareHost.yml -i labhost -u lab3 -k -K
 ansible-playbook prepareHost.yml -i labhost
 sleep 10
