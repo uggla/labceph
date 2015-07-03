768b68ba99887b6cd81a95b2cb5992eb67ac5345
cedric milesi <cedric.milesi@hp.com>
2015-07-03 02:19:46 +0200

    added template for default pool in libvirt

--- clean_vm.sh
+++ clean_vm.sh
@@ -2,3 +2,4 @@
 
 for i in $(virsh list --all | grep ceph | awk '{print $2}'); do virsh undefine $i; done
 for i in $(virsh list --all | grep ceph | awk '{print $2}'); do virsh destroy $i; done
+virsh vol-list default | grep "_osd[0-9].img" | awk '{print $1}' | while read vol; do virsh vol-delete $vol --pool=default; done
