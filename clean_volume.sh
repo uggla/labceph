4dd9208c161dc342094b9501f05a88914aba8e06
cedric milesi <cedric.milesi@hp.com>
2015-07-03 07:22:26 +0200

    force removal of image even if libvirt non functional

--- clean_volume.sh
+++ clean_volume.sh
@@ -9,3 +9,5 @@ virsh detach-disk --domain cephosd2 vdc
 virsh detach-disk --domain cephosd3 vdc
 
 for i in $(virsh vol-list default | grep ceph | awk '{print $1}'); do virsh vol-delete --vol $i --pool default; done
+#really clean up directory
+rm -rf /var/lib/libvirt/images/*
