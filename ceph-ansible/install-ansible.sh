15c6112a08ef82ebd90ed24ae477c63448f95489
cedric milesi <cedric.milesi@hpe.com>
2015-12-22 14:12:04 +0100

    added ceph-ansible project

---
+++ ceph-ansible/install-ansible.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# THIS SCRIPT INSTALLS ANSIBLE
+
+if [[ $EUID -ne 0 ]]; then
+    echo "You are NOT running this script as root."
+    echo "You should."
+    echo "Really."
+    exit 1
+fi
+
+if [[ -x $(which lsb_release 2>/dev/null) ]]; then
+  os_VENDOR=$(lsb_release -i -s)
+  os_VERSION=$(lsb_release -c -s)
+  if [[ "Debian" =~ $os_VENDOR ]]; then
+    apt-get update
+    apt-get install python-pip python-dev git build-essential -y
+    pip install PyYAML jinja2 paramiko
+    git clone https://github.com/ansible/ansible.git
+    cd ansible
+    make install
+    mkdir /etc/ansible
+  elif [[ "Ubuntu" =~ $os_VENDOR ]]; then
+      add-apt-repository ppa:ansible/ansible
+      apt-get update
+      apt-get install -y ansible
+  fi
+elif [[ -r /etc/redhat-release ]]; then
+  yum install -y epel-release
+  yum install -y ansible
+fi
